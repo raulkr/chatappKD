e51fe5065f002f3edd2191aea74d6d9631cb31bd
rahulasthana <rahulasthana@users.noreply.github.com>
2020-11-17 12:21:12 +0530

    Initial commit

---
+++ app/src/main/java/com/rahul/chatapp/Adapter/MessageAdapter.java
@@ -0,0 +1,83 @@
+package com.rahul.chatapp.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.rahul.chatapp.MessageActivity;
+import com.rahul.chatapp.Model.User;
+import com.rahul.chatapp.R;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+
+    public static final int MSG_TYPE_LEFT = 0;
+    public static final int MSG_TYPE_RIGHT = 1;
+    private Context mContext;
+    private List<User> mUser;
+
+    public MessageAdapter(Context mContext, List<User> mUsers){
+        this.mUser = mUsers;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public MessageAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new MessageAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageAdapter.ViewHolder holder, int position) {
+
+        User user = mUser.get(position);
+        holder.username.setText(user.getUsername());
+        if (user.getImageURL().equals("default")){
+            holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+        }else {
+            Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid", user.getId());
+                mContext.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUser.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView username;
+        public ImageView profile_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+
+        }
+    }
+
+
+}
+
